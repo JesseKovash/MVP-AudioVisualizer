820c351da64e592c0be1853c5a87380a4c9210d5
Jesse Kovash <jessekovash@gmail.com>
2022-08-05 08:39:43 -0500

    tried build path for server

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 const path = require("path");
 // app.use(express.static(path.join(__dirname, "..", "public")));
-app.use(express.static(path.resolve(__dirname, '../public')))
+app.use(express.static(path.resolve(__dirname, '../build')))
 app.use(express.json());
 app.use(cors());
 
